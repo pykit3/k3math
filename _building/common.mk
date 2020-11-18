26307f2638448a4265be8c77a7e9d9db9df242b6
drdr xp <drdr.xp@gmail.com>
2020-11-19 00:08:15 +0800

    use _building/build_setup.py to release

--- _building/common.mk
+++ _building/common.mk
@@ -1,9 +1,15 @@
-all: test
-	# readme
-	make -C _building
-	make -C docs html
+all: test readme doc
 
 .PHONY: test
 test:
 	sudo env "PATH=$$PATH" UT_DEBUG=0 PYTHONPATH="$$(cd ..; pwd)" python -m unittest discover -c --failfast -s .
+
+doc:
+	make -C docs html
+
+readme:
+	python _building/build_readme.py
+
+release:
+	python _building/build_setup.py
 
